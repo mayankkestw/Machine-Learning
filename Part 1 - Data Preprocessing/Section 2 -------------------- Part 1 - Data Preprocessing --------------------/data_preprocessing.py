10b8f8f7528903878815468ab57dfb183f57fa7b
Mayank Kestwal <mayankkestwal8@gmail.com>
2018-10-09 04:20:22 +0530

    Feature Scaling

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
@@ -28,3 +28,9 @@ y = labelencoder_Y.fit_transform(y)
 #Splitting data
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(x,y, test_size=0.2, random_state=0)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
