3dfb1a1e11e0b10f705e225c2c9dd1b3dddb1646
Mayank Kestwal <mayankkestwal8@gmail.com>
2018-10-10 00:54:43 +0530

    Started Simple Linear Regression

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
@@ -11,7 +11,7 @@ x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,3].values
 
 #Missing values
-from sklearn.preprocessing import Imputer
+'''from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values='NaN',strategy='mean',axis=0)
 imputer = imputer.fit(x[:,1:3])
 x[:,1:3] = imputer.transform(x[:,1:3])
@@ -23,14 +23,14 @@ x[:,0]=labelencoder_X.fit_transform(x[:,0])
 oneHotEncoder = OneHotEncoder(categorical_features=[0])
 x=oneHotEncoder.fit_transform(x).toarray()
 labelencoder_Y = LabelEncoder()
-y = labelencoder_Y.fit_transform(y)
+y = labelencoder_Y.fit_transform(y)'''
 
 #Splitting data
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(x,y, test_size=0.2, random_state=0)
 
 #Feature Scaling
-from sklearn.preprocessing import StandardScaler
+'''from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+X_test = sc_X.transform(X_test)'''
---
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression.py
@@ -0,0 +1,21 @@
+#Simple Linear Regression
+
+#import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Read data
+dataset = pd.read_csv('Salary_Data.csv')
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,1].values
+
+#Splitting data
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(x,y, test_size=1/3, random_state=0)
+
+#Feature Scaling
+'''from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)'''
