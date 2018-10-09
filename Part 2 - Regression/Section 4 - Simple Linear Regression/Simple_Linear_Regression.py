4ce70ca6ef45f6e140a11f2d6ce2c55f4200e307
Mayank Kestwal <mayankkestwal8@gmail.com>
2018-10-10 01:43:51 +0530

    Visualisation

--- Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression.py
@@ -27,3 +27,19 @@ regressor.fit(X_train,Y_train)
 
 #Predict the test set results
 y_pred = regressor.predict(X_test)
+
+#Visualising the training set
+plt.scatter(X_train,Y_train,color='red')
+plt.plot(X_train, regressor.predict(X_train),color='blue')
+plt.title('Salary vs Experience(Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualising the Test set
+plt.scatter(X_test,Y_test,color='red')
+plt.plot(X_train, regressor.predict(X_train),color='blue')
+plt.title('Salary vs Experience(Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
