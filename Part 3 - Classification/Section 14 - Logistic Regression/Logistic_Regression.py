1a3c5165485c78836e65ba3bc90b5eb68e18fa8f
Mayank Kestwal <mayankkestwal8@gmail.com>
2018-11-09 16:08:49 +0530

    Logistic Regression Visualised

--- Part 3 - Classification/Section 14 - Logistic Regression/Logistic_Regression.py
+++ Part 3 - Classification/Section 14 - Logistic Regression/Logistic_Regression.py
@@ -32,3 +32,33 @@ y_pred = classifier.predict(X_test)
 #Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test,y_pred)
+
+#Visualising the Training set results
+from matplotlib.colors import ListedColormap
+x_set, y_set = X_train, y_train
+x1, x2 = np.meshgrid(np.arange(start=x_set[:,0].min()-1,stop=x_set[:,0].max()+1,step=0.01),np.arange(start=x_set[:,1].min()-1,stop=x_set[:,1].max()+1,step=0.01))
+plt.contourf(x1,x2,classifier.predict(np.array([x1.ravel(),x2.ravel()]).T).reshape(x1.shape),alpha=0.75, cmap= ListedColormap('red','green'))
+plt.xlim(x1.min(),x1.max())
+plt.ylim(x2.min(),x2.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set==j,0],x_set[y_set==j,1],c=ListedColormap(('red','green'))(i),label=j)
+plt.title('Logistic Regression(Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+#Visualising the test set results
+from matplotlib.colors import ListedColormap
+x_set, y_set = X_test, y_test
+x1, x2 = np.meshgrid(np.arange(start=x_set[:,0].min()-1,stop=x_set[:,0].max()+1,step=0.01),np.arange(start=x_set[:,1].min()-1,stop=x_set[:,1].max()+1,step=0.01))
+plt.contourf(x1,x2,classifier.predict(np.array([x1.ravel(),x2.ravel()]).T).reshape(x1.shape),alpha=0.75, cmap= ListedColormap('red','green'))
+plt.xlim(x1.min(),x1.max())
+plt.ylim(x2.min(),x2.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set==j,0],x_set[y_set==j,1],c=ListedColormap(('red','green'))(i),label=j)
+plt.title('Logistic Regression(Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
