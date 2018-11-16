ca398034811f8f9578afccd60edc3b07f970f1b4
Mayank Kestwal <mayankkestwal8@gmail.com>
2018-11-17 02:11:58 +0530

    minor change

--- Part 3 - Classification/Section 13 -------------------- Part 3 - Classification --------------------/Classification_Template.py
+++ Part 3 - Classification/Section 13 -------------------- Part 3 - Classification --------------------/Classification_Template.py
@@ -34,12 +34,12 @@ cm = confusion_matrix(y_test,y_pred)
 from matplotlib.colors import ListedColormap
 x_set, y_set = X_train, y_train
 x1, x2 = np.meshgrid(np.arange(start=x_set[:,0].min()-1,stop=x_set[:,0].max()+1,step=0.01),np.arange(start=x_set[:,1].min()-1,stop=x_set[:,1].max()+1,step=0.01))
-plt.contourf(x1,x2,classifier.predict(np.array([x1.ravel(),x2.ravel()]).T).reshape(x1.shape),alpha=0.75, cmap= ListedColormap('red','green'))
+plt.contourf(x1,x2,classifier.predict(np.array([x1.ravel(),x2.ravel()]).T).reshape(x1.shape),alpha=0.75, cmap= ListedColormap(('red','green')))
 plt.xlim(x1.min(),x1.max())
 plt.ylim(x2.min(),x2.max())
 for i,j in enumerate(np.unique(y_set)):
     plt.scatter(x_set[y_set==j,0],x_set[y_set==j,1],c=ListedColormap(('red','green'))(i),label=j)
-plt.title('Logistic Regression(Training set)')
+plt.title('Model(Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -49,12 +49,12 @@ plt.show()
 from matplotlib.colors import ListedColormap
 x_set, y_set = X_test, y_test
 x1, x2 = np.meshgrid(np.arange(start=x_set[:,0].min()-1,stop=x_set[:,0].max()+1,step=0.01),np.arange(start=x_set[:,1].min()-1,stop=x_set[:,1].max()+1,step=0.01))
-plt.contourf(x1,x2,classifier.predict(np.array([x1.ravel(),x2.ravel()]).T).reshape(x1.shape),alpha=0.75, cmap= ListedColormap('red','green'))
+plt.contourf(x1,x2,classifier.predict(np.array([x1.ravel(),x2.ravel()]).T).reshape(x1.shape),alpha=0.75, cmap= ListedColormap(('red','green')))
 plt.xlim(x1.min(),x1.max())
 plt.ylim(x2.min(),x2.max())
 for i,j in enumerate(np.unique(y_set)):
     plt.scatter(x_set[y_set==j,0],x_set[y_set==j,1],c=ListedColormap(('red','green'))(i),label=j)
-plt.title('Logistic Regression(Test set)')
+plt.title('Model(Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
