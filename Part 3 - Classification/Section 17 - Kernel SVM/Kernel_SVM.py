fc963aaf51b2fac1a203df0afb2d38c37ebdec9a
Mayank Kestwal <mayankkestwal8@gmail.com>
2018-11-17 02:08:17 +0530

    minor changes

--- Part 3 - Classification/Section 17 - Kernel SVM/Kernel_SVM.py
+++ Part 3 - Classification/Section 17 - Kernel SVM/Kernel_SVM.py
@@ -35,7 +35,7 @@ cm = confusion_matrix(y_test,y_pred)
 from matplotlib.colors import ListedColormap
 x_set, y_set = X_train, y_train
 x1, x2 = np.meshgrid(np.arange(start=x_set[:,0].min()-1,stop=x_set[:,0].max()+1,step=0.01),np.arange(start=x_set[:,1].min()-1,stop=x_set[:,1].max()+1,step=0.01))
-plt.contourf(x1,x2,classifier.predict(np.array([x1.ravel(),x2.ravel()]).T).reshape(x1.shape),alpha=0.75, cmap= ListedColormap('red','green'))
+plt.contourf(x1,x2,classifier.predict(np.array([x1.ravel(),x2.ravel()]).T).reshape(x1.shape),alpha=0.75, cmap= ListedColormap(('red','green')))
 plt.xlim(x1.min(),x1.max())
 plt.ylim(x2.min(),x2.max())
 for i,j in enumerate(np.unique(y_set)):
